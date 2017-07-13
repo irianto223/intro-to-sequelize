4cd25b268b89e1fcafe64888203d2fbb482e2b0e
anto <irianto223@gmail.com>
2017-07-13 23:04:12 +0700

    Validation DONE

--- routers/student.js
+++ routers/student.js
@@ -7,7 +7,6 @@ var studentModel = require('../models')
 router.get('/students', (req,res) => {
   studentModel.Student.findAll()
   .then((data) => {
-  // projects will be an array of all Project instances
     res.render('student', {dataStudent: data})
   })
 })
@@ -17,12 +16,28 @@ router.get('/students/add', (req,res) => {
 })
 
 router.post('/students/add/', (req,res) => {
-  studentModel.Student.create({ first_name: req.body.first_name, last_name: req.body.last_name, email: req.body.email, jurusan: req.body.jurusan, createdAt: new Date(), updatedAt: new Date() })
-  .then(() => {
-    res.redirect('/students')
-  })
-  .catch((err) => {
-    res.redirect('/students/add')
+  // studentModel.Student.create({ first_name: req.body.first_name, last_name: req.body.last_name, email: req.body.email, jurusan: req.body.jurusan, createdAt: new Date(), updatedAt: new Date() })
+  // .then(() => {
+  //   res.redirect('/students')
+  // })
+  // .catch((err) => {
+  //   // res.redirect('/students/add')
+  //   res.send('email format is incorrect.')
+  // })
+  studentModel.Student.findOne({where: {email: req.body.email}})
+  .then(data => {
+    if(!data || req.body.email === req.body.emailPembanding) {
+      studentModel.Student.create({ first_name: req.body.first_name, last_name: req.body.last_name, email: req.body.email, jurusan: req.body.jurusan, createdAt: new Date(), updatedAt: new Date() })
+      .then(() => {
+        res.redirect('/students')
+      })
+      .catch(err => {
+        res.send('email format is incorrect.')
+      })
+    }
+    else {
+      res.send('email sudah ada.')
+    }
   })
 })
 
@@ -37,19 +52,34 @@ router.get('/students/edit/:id', (req,res) => {
   studentModel.Student.findById(req.params.id)
   .then((data) => {
     res.render('edit_student', {dataEdit: data})
+  })
 })
 
 router.post('/students/edit/:id', (req,res) => {
-  studentModel.Student.update({first_name: req.body.first_name, last_name: req.body.last_name, email: req.body.email, jurusan: req.body.jurusan}, {where: {id: req.params.id}})
-  .then(() => {
-    res.redirect('/students')
-  })
-  .catch((err) => {
-    res.redirect(`/students/edit/${req.params.id}`)
-    // res.send('tes')
+  // studentModel.Student.update({first_name: req.body.first_name, last_name: req.body.last_name, email: req.body.email, jurusan: req.body.jurusan}, {where: {id: req.params.id}})
+  // .then(() => {
+  //   res.redirect('/students')
+  // })
+  // .catch((err) => {
+  //   // res.redirect(`/students/edit/${req.params.id}`)
+  //   res.send('email format is incorrect.')
+  // })
+  studentModel.Student.findOne({where: {email: req.body.email}})
+  .then(data => {
+    if(!data || req.body.email === req.body.emailPembanding) {
+      studentModel.Student.update({ first_name: req.body.first_name, last_name: req.body.last_name, email: req.body.email, jurusan: req.body.jurusan, updatedAt: new Date()}, { where: { id: req.params.id }})
+      .then(() => {
+        res.redirect('/students')
+      })
+      .catch(err => {
+        res.send('email format is incorrect.')
+      })
+    }
+    else {
+      res.send('email sudah ada.')
+    }
   })
 })
 
-})
 
 module.exports = router
