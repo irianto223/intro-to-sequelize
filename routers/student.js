7efc29c05cd2d8150aeb420b551645a0cad46c72
anto <irianto223@gmail.com>
2017-07-13 21:18:42 +0700

    validasi format email

--- routers/student.js
+++ routers/student.js
@@ -6,7 +6,7 @@ var studentModel = require('../models')
 
 router.get('/students', (req,res) => {
   studentModel.Student.findAll()
-  .then(data => {
+  .then((data) => {
   // projects will be an array of all Project instances
     res.render('student', {dataStudent: data})
   })
@@ -18,28 +18,35 @@ router.get('/students/add', (req,res) => {
 
 router.post('/students/add/', (req,res) => {
   studentModel.Student.create({ first_name: req.body.first_name, last_name: req.body.last_name, email: req.body.email, jurusan: req.body.jurusan, createdAt: new Date(), updatedAt: new Date() })
-  .then(data => {
+  .then(() => {
     res.redirect('/students')
+  })
+  .catch((err) => {
+    res.redirect('/students/add')
   })
 })
 
 router.get('/students/delete/:id', (req,res) => {
   studentModel.Student.destroy({where: {id: req.params.id}})
-  .then(data => {
+  .then(() => {
     res.redirect('/students')
   })
 })
 
 router.get('/students/edit/:id', (req,res) => {
   studentModel.Student.findById(req.params.id)
-  .then(data => {
+  .then((data) => {
     res.render('edit_student', {dataEdit: data})
 })
 
 router.post('/students/edit/:id', (req,res) => {
   studentModel.Student.update({first_name: req.body.first_name, last_name: req.body.last_name, email: req.body.email, jurusan: req.body.jurusan}, {where: {id: req.params.id}})
-  .then(data => {
+  .then(() => {
     res.redirect('/students')
+  })
+  .catch((err) => {
+    res.redirect(`/students/edit/${req.params.id}`)
+    // res.send('tes')
   })
 })
 
